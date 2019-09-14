9642e32bcc37689ef67c99f78c7f07d1ae5c7e4d
VTC <vtchris@hotmail.com>
2019-09-14 11:59:28 -0400

    Enable enroll page, workflow enroll > login > wishlist

--- client/src/pages/Enroll.js
+++ client/src/pages/Enroll.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import Enrollcard from "../components/Enrollcard"
+import API from "../utils/API";
 
-  function Login() {
-  return (
-    <div className="container">
-      <Enrollcard />
-    </div>
-  );
+class Enroll extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  };
+  handleOnUpdate = e => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+  handleFormSubmit = e => {
+    e.preventDefault();
+    
+    if (this.state.email && this.state.password) {
+      API.saveUser({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        password: this.state.password
+      }).then(res => window.location.replace("/login"))
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Enrollcard handleOnUpdate={this.handleOnUpdate} handleFormSubmit={this.handleFormSubmit} />
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default Enroll;
